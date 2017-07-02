225ab61cdbf46925752730f4927483dcc0cd4c51
Jordi Corbilla <jordi.coll.corbilla@gmail.com>
2017-07-02 20:33:53 +0100

    Add JobScheduler files

---
+++ JobScheduler.runner/JobScheduler.runner/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JobScheduler.runner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
